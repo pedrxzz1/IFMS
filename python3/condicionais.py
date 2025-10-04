fb4c94195b6c03a90deef596c01191f20c6a5cd7
pedrxzz1 <sorrisophflu@gmail.com>
2025-10-03 22:39:28 -0400

    simples

--- python3/condicionais.py
+++ python3/condicionais.py
@@ -6,3 +6,12 @@ __version__ = "x.x.x"
 __author__ = "Pedro Henrique"
 __license__ = "unlicense"
 
+name = input("Qual e o seu nome? ")
+
+if name == "Pedro":
+    print("Seu nome e INCRIVEL!!!")
+elif name == "Gustavo" or name == "Maria":
+    print("Seu nome e bem popular no Brasil. ")
+else:
+    print("Que bosta. ")
+
