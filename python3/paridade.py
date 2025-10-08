6c26cb2ad6b34d8bb161cb60951ce7796263020e
pedrxzz1 <sorrisophflu@gmail.com>
2025-10-08 14:12:04 -0400

    simples

---
+++ python3/paridade.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Exercicios de condicionais
+"""
+
+__version__ = "x.x.x"
+__author__ = "Pedro Henrique"
+__license__ = "unlicense"
+
+for n in range(1, 51):
+    if n % 2 == 0:
+        print(n)
+
