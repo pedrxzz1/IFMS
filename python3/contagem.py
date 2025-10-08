730aaaa1fee4aecebc8edbabe3c2a0a0ecc15917
pedrxzz1 <sorrisophflu@gmail.com>
2025-10-08 13:57:17 -0400

    simples

---
+++ python3/contagem.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Exercicios de condicionais
+"""
+
+__version__ = "x.x.x"
+__author__ = "Pedro Henrique"
+__license__ = "unlicense"
+
+from time import sleep
+
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(1)
+    
+print("Feliz ano novo! \U0001F387")
