9a33a668cec384ef4fc8944462fbc586fe230a48
pedrxzz1 <sorrisophflu@gmail.com>
2025-10-08 13:42:42 -0400

    simples

--- python3/for.py
+++ python3/for.py
@@ -5,3 +5,10 @@
 __version__ = "x.x.x"
 __author__ = "Pedro Henrique"
 __license__ = "unlicense"
+
+s = 0
+
+for c in range(0,10):
+    n = int(input("Digite um numero: "))
+    s += n
+print("O somatorio da {}.".format(s))
